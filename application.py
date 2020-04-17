692e4531d46ea98150e9ec2eacdd7e3947c3b4a8
Prem Kumar <prembammidi25@msitprogram.net>
2020-04-17 20:10:18 +0530

    Data bases Commit

--- application.py
+++ application.py
@@ -1,13 +1,17 @@
+import os
+
 from flask import Flask, render_template, request, session
 from flask_session import Session
+from models import *
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATION "] = False
 Session(app)
-
-data = []
+db.init_app(app)
 
 @app.route("/register")
 def register():
@@ -15,16 +19,20 @@ def register():
 
 @app.route("/hello", methods=["GET", "POST"])
 def hello():
+    db.create_all()
     if request.method == "GET":
         return "Please register into the web site"
     else :
         first_name = request.form.get("first-name")
         last_name = request.form.get("last-name")
-        name = first_name + " " + last_name
-        dob = request.form.get("date-of-birth")
-        gender = request.form.get("gender")
-        email = request.form.get("email")
-        phone = request.form.get("Phone-number")
-        details = name +"\n"+ dob +"\n"+ gender +"\n"+ email + "\n" + phone
-        print(details)
+        full_name = first_name + " " + last_name
+        birth = request.form.get("date-of-birth")
+        gen = request.form.get("gender")
+        mail = request.form.get("email")
+        ph = request.form.get("Phone-number")
+        pswd = request.form.get("password")
+        details = full_name +"\n"+ birth +"\n"+ gen +"\n"+ mail + "\n" + ph
+        user1 = Users(name=full_name, dob=birth, gender=gen, email=mail, phone=ph, password=pswd)
+        db.session.add(user1)
+        db.session.commit()
         return render_template("hello.html", name = details)
---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    __tablename__ = "Users"
+    name = db.Column(db.String, nullable=False)
+    dob = db.Column(db.String, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, primary_key=True)
+    phone = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, primary_key=True)
