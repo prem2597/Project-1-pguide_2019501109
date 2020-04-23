926b72dc8269d6f121b3c50440fee128aef3fd89
Surya <55698248+viswa9688@users.noreply.github.com>
2020-04-23 18:25:13 +0530

    search_Added
    search added.
    probelm:
    1. Need to integrate with rest of the code

--- import.py
+++ import.py
@@ -8,6 +8,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import render_template, request, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+#change made by viswa
+import flask_whooshalchemy as wa
 from models import *
 
 app1 = Flask(__name__)
@@ -18,13 +20,18 @@ Session(app1)
 db = SQLAlchemy()
 
 app1.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app1.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# change made by viswa
+app1.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+#change made by viswa
+app1.config['DEBUG']=True
+app1.config['WHOOSH_BASE']='whoosh'
 app1.app_context().push()
 
 db.init_app(app1)
 
 class Books(db.Model):
     __tablename__ = "Books"
+    __searchable__=['isbn','title','author','year']
     isbn = db.Column(db.String, primary_key=True)
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
@@ -49,6 +56,9 @@ def main():
             db.session.add(book)
         i = i + 1
     db.session.commit()
+def search():
+    wa.whoosh_index(app1,Books)
+    Books.query.whoosh_search('Aztec').all()
 
 if __name__ == "__main__":
     main()
